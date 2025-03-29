c5e97c9253a8f27122a22f66382a0430d9d6663c
amadyira25@amherst.edu <amadyira25@amherst.edu>
2025-03-29 19:02:50 -0400

    Add meal specific API

---
+++ food-waste-app/pages/api/dinner-menu.js
@@ -0,0 +1,18 @@
+// pages/api/menu.js
+export default async function handler(req, res) {
+    const apiUrl = process.env.NEXT_PUBLIC_DINNER_MENU_API_URL;
+    console.log('API URL:', apiUrl);
+
+    try {
+        const response = await fetch(apiUrl,{
+            method: 'GET',
+        });
+        if (!response.ok) {
+            throw new Error('Failed to fetch data from API');
+        }
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
